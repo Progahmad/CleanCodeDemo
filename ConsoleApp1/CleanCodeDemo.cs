8b93708544f3f9fab09f336e8888960d66fc0100
ahmed Samir <prog.ahmad@hotmail.com>
2023-02-04 22:01:01 +0200

    Add project files.

---
+++ ConsoleApp1/CleanCodeDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class CleanCodeDemo
+    {
+        int exchangeRate;
+
+        public bool changename(List<string> name)
+        {
+            bool fl = true;
+            foreach (var n in name)
+            {
+                if (fl)
+                {
+                    if (GoesOrNoGoes(n))
+                    {
+                        fl = false;
+                    }
+                }
+            }
+            return fl;
+        }
+        private bool GoesOrNoGoes(string emp)
+        {
+            return string.IsNullOrEmpty(emp);
+        }
+
+    }
+}
